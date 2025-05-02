9cdff5c315ff6f61930cadbbc8b7348554f081d2
rainlizard <15337628+rainlizard@users.noreply.github.com>
2025-05-02 16:22:10 +1000

    hidream update

--- EasyQuantizationGUI.py
+++ EasyQuantizationGUI.py
@@ -229,7 +229,7 @@ def main():
     quantize_label = tk.Label(quantize_frame, text="Quantize Level:")
     quantize_label.pack(side=tk.LEFT)
 
-    quantize_levels = ["Q2_K", "Q3_K_S", "Q4_0", "Q4_1", "Q4_K_S", "Q5_0", "Q5_1", "Q5_K_S", "Q6_K", "Q8_0", "F16"]
+    quantize_levels = ["Q2_K", "Q2_K_S", "Q3_K", "Q3_K_L", "Q3_K_M", "Q3_K_S", "Q4_0", "Q4_1", "Q4_K", "Q4_K_M", "Q4_K_S", "Q5_0", "Q5_1", "Q5_K", "Q5_K_M", "Q5_K_S", "Q6_K", "Q8_0", "F16", "BF16", "F32"]
     quantize_level_var = tk.StringVar(root)
     quantize_level_var.set("Q8_0")  # Set default value to Q8_0
 
